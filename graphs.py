d02e3cdcbe184822ae9fa6121a7b00e9bbe1b8c0
rsj-96 <rachel.johnston1996@gmail.com>
2025-03-14 15:23:34 +0000

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -86,7 +86,9 @@ if file:
     if not df.empty: # command checks if the dataframe is empty or not, if it's not it will progress with plotting the barchart
     
         fig, ax = plt.subplots()
-        ax = sns.barplot(x='Solvent', y='Solubility (mg/ml)', hue='Temperature', data=df, palette=colours, fontproperties=font_prop)
+        ax = sns.barplot(x='Solvent', y='Solubility (mg/ml)', hue='Temperature', data=df, palette=colours)
+        plt.xlabel(label_3, fontproperties=font_prop)
+        plt.ylabel("Solubility (mg/ml)", fontproperties=font_prop)
         plt.xticks(rotation=90, fontproperties=font_prop)
         plt.yticks(fontproperties=font_prop)
         plt.title(title, fontproperties=font_prop)
