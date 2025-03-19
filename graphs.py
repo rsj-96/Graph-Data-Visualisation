27f920531089f11fb74c065dcb5f2b15891db8fe
rsj-96 <rachel.johnston1996@gmail.com>
2025-03-19 14:17:49 +0000

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -9,7 +9,7 @@ import matplotlib.font_manager as fm
 import seaborn as sns
 import os
 
-print(os.getcwd()) # finds files path of repository
+st.write(print(os.getcwd())) # finds files path of repository
 
 # Name of Script
 st.title('Graphs for Reaction Screens and Solubility Studies')  # Replace with your script name
