1334be1f00973c5e482c6d8776d23a0fb1965476
rsj-96 <rachel.johnston1996@gmail.com>
2025-03-25 11:46:28 +0000

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -523,7 +523,7 @@ else:
             ax = axes[r]
             wedges, texts, autotexts = ax.pie(row[1:], autopct = '%1.1f%%', colors=colours, startangle=360)
             # texts = labels of each wedge, wedges = objects representing pie slices, autotexts = text annotations inside the wedges
-            ax.set_title(row[0], fontsize=30, fontname=font_prop)
+            ax.set_title(row[0], fontsize=30, fontproperties=font_prop)
             wedges.extend(wedges) # appends individual wedges into wedges list and is used for making the global legend
             
             
@@ -533,7 +533,7 @@ else:
                         
                 # Set label size and position
                 autotext.set_fontsize(size_label)
-                autotext.set_fontname(font_prop)
+                autotext.set_fontproperties(font_prop)
         
                 # Adjust position: inside if large enough, outside if too small
                 if value > 5.5: 
@@ -556,8 +556,7 @@ else:
         
         fig.legend(wedges[:len(labels)], labels, loc="upper left",bbox_to_anchor=(1,1), fontsize=30)
 
-        plt.subplots_adjust(hspace=0.5, wspace=0.5)
-        plt.tight_layout() # change distance between pie plots
+        plt.tight_layout(h_pad=5) # change distance between pie plots
         
         st.pyplot(fig)
 
